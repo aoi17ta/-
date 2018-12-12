189ed00c1bc16671247d45351ba0a95552cb7803
chiikama <eru.birdsea@gmail.com>
2018-12-12 11:36:42 +0900

    Scene入れて動いたら最高

---
+++ yakisoba/Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    Animator anim;
+
+    public string newGameSceneName;
+    public int quickSaveSlotID;
+
+    [Header("Options Panel")]
+    public GameObject MainOptionsPanel;
+    public GameObject StartGameOptionsPanel;
+    public GameObject GamePanel;
+    public GameObject ControlsPanel;
+    public GameObject GfxPanel;
+    public GameObject LoadGamePanel;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+        //new key
+        PlayerPrefs.SetInt("quickSaveSlot", quickSaveSlotID);
+    }
+
+    #region Open Different panels
+
+    public void openOptions()
+    {
+        //enable respective panel
+        MainOptionsPanel.SetActive(true);
+        StartGameOptionsPanel.SetActive(false);
+
+        //play anim for opening main options panel
+        anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+       
+    }
+
+    public void openStartGameOptions()
+    {
+        //enable respective panel
+        MainOptionsPanel.SetActive(false);
+        StartGameOptionsPanel.SetActive(true);
+
+        //play anim for opening main options panel
+        anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+        
+    }
+    public void newGame()
+    {
+        if (!string.IsNullOrEmpty(newGameSceneName))
+            SceneManager.LoadScene(newGameSceneName);
+        else
+            Debug.Log("Please write a scene name in the 'newGameSceneName' field of the Main Menu Script and don't forget to " +
+                "add that scene in the Build Settings!");
+    }
+    #endregion
+
+
+
+  
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    #region Sounds
+    public void playHoverClip()
+    {
+       
+    }
+
+    void playClickSound() {
+
+    }
+
+
+    #endregion
+}
